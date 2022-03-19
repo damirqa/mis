e33bea366a7b718ed4069d5da54a8a091e1ac68a
damirqa <damirshaniyazov@gmail.com>
2022-03-19 20:25:29 +0500

    auth module

--- frontend/src/modules/authentication/ForgotPassword.jsx
+++ frontend/src/modules/authentication/ForgotPassword.jsx
@@ -3,14 +3,8 @@ import {Link} from "react-router-dom";
 import Input from "../../components/common/fields/Input";
 import InputWithLink from "../../components/common/fields/InputWithLink";
 
-
-
-const Authentication = () => {
-    const [data, setData] = useState({login: '', password: ''});
-
-    const handleChange = e => {
-        setData({...data, [e.target.id]: e.target.value })
-    }
+const ForgotPassword = () => {
+    const [email, setEmail] = useState('');
 
     const handleSubmit = () => {
         console.log(1)
@@ -20,15 +14,12 @@ const Authentication = () => {
         <div className='flex flex-row min-h-screen items-center justify-center bg-blue-50'>
             <div className='bg-white text-lg font-medium p-10 mb-10 w-full max-w-md'>
                 <div className='pb-9 text-center'>
-                    <h1 className='text-4xl font-bold'>Sign in to Shaniyazov</h1>
-                    <div>Don't have an account? <Link to='/register' className='text-blue-600 hover:underline'>Register here</Link></div>
+                    <h1 className='text-4xl font-bold'>Forgot password?</h1>
+                    <div>Changed your mind? <Link to='/login' className='text-blue-600 hover:underline'>Sign in here</Link></div>
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-6">
-                        <Input name='login' label='Login' data={data} required={true} onChange={handleChange}/>
-                    </div>
-                    <div className="mb-6">
-                        <InputWithLink name='password' type='password' label='Password' data={data} required={true} link='/forgot-password' textLink='Forgot password?' onChange={handleChange}/>
+                        <Input name='email' label='Email Address' data={email} required={true} onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <button type="submit"
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg w-full px-5 py-2.5 text-center">Submit
@@ -39,4 +30,4 @@ const Authentication = () => {
     );
 };
 
-export default Authentication;
+export default ForgotPassword;
--- frontend/src/modules/authentication/PasswordReset.jsx
+++ frontend/src/modules/authentication/PasswordReset.jsx
@@ -1,34 +1,30 @@
 import React, {useState} from 'react';
 import {Link} from "react-router-dom";
 import Input from "../../components/common/fields/Input";
-import InputWithLink from "../../components/common/fields/InputWithLink";
 
-
-
-const Authentication = () => {
-    const [data, setData] = useState({login: '', password: ''});
+const PasswordReset = () => {
+    const [data, setData] = useState({password: '', repeat_password: ''});
 
     const handleChange = e => {
         setData({...data, [e.target.id]: e.target.value })
     }
 
-    const handleSubmit = () => {
-        console.log(1)
+    const handleSubmit = e => {
+        e.preventDefault();
     }
 
     return (
         <div className='flex flex-row min-h-screen items-center justify-center bg-blue-50'>
             <div className='bg-white text-lg font-medium p-10 mb-10 w-full max-w-md'>
                 <div className='pb-9 text-center'>
-                    <h1 className='text-4xl font-bold'>Sign in to Shaniyazov</h1>
-                    <div>Don't have an account? <Link to='/register' className='text-blue-600 hover:underline'>Register here</Link></div>
+                    <h1 className='text-4xl font-bold'>Password Reset</h1>
                 </div>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <div className="mb-6">
-                        <Input name='login' label='Login' data={data} required={true} onChange={handleChange}/>
+                        <Input name='password' type='password' label='Password' data={data} required={true} onChange={handleChange}/>
                     </div>
                     <div className="mb-6">
-                        <InputWithLink name='password' type='password' label='Password' data={data} required={true} link='/forgot-password' textLink='Forgot password?' onChange={handleChange}/>
+                        <Input name='repeat_password' type='password' label='Repeat Password' data={data} required={true} onChange={handleChange}/>
                     </div>
                     <button type="submit"
                             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg w-full px-5 py-2.5 text-center">Submit
@@ -39,4 +35,4 @@ const Authentication = () => {
     );
 };
 
-export default Authentication;
+export default PasswordReset;
